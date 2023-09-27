f17f08bf7f41b801a22c378e39b383342ba6c3c7
DmytroKondrashov <kondrashovd101@gmail.com>
2023-09-27 22:21:25 +0300

    update user model with devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :rememberable, :validatable, :timeoutable
+  devise :recoverable if %w[development test].include?(Rails.env) || (ENV["DISABLE_PASSWORD_RECOVERY"] != "true" && ENV["MAILGUN_SMTP_PASSWORD"].present?)
+
 end
