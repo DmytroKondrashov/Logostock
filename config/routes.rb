5a197c8a7757e3c41a122fc5abf54cd189da84ab
DmytroKondrashov <kondrashovd101@gmail.com>
2023-09-28 11:48:46 +0300

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   namespace :admin do
-      resources :users
+    root to: "companies#index"
+    resources :users
 
-      root to: "users#index"
-    end
+    # root to: "users#index"
+  end
+  
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
