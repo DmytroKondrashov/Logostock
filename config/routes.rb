3c4a7ae3f7d4b6c6dcb471ae8f6efe1921be6119
DmytroKondrashov <kondrashovd101@gmail.com>
2023-09-28 12:08:56 +0300

    add resources :companies to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   namespace :admin do
     root to: "companies#index"
+    
+    resources :companies
     resources :users
 
     # root to: "users#index"
   end
-  
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
