78485da69db30bba29934b9009f23dc9c174a902
DmytroKondrashov <kondrashovd101@gmail.com>
2023-10-03 19:42:02 +0300

    fix active record migration version

--- db/migrate/20231003163958_add_unique_indexes_to_tags_tables.rb
+++ db/migrate/20231003163958_add_unique_indexes_to_tags_tables.rb
@@ -1,4 +1,4 @@
-class AddUniqueIndexesToTagsTables < ActiveRecord::Migration[6.1]
+class AddUniqueIndexesToTagsTables < ActiveRecord::Migration[7.0]
   def change
     add_index :job_functions, :name, unique: true
     add_index :asset_classes, :name, unique: true
