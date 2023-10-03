4f6ecbf1dc9ec90d0ad68a71cdd99a55248a0acf
DmytroKondrashov <kondrashovd101@gmail.com>
2023-10-03 19:46:16 +0300

    add logo to company

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_03_164113) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_03_164536) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_03_164113) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "logotype"
   end
 
   create_table "companies_job_functions", force: :cascade do |t|
