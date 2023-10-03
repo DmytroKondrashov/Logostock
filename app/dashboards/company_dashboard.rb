65e90df364b9988f4de2fcc6a3557c8d310bcc23
DmytroKondrashov <kondrashovd101@gmail.com>
2023-10-03 18:06:43 +0300

    update company dashboard

--- app/dashboards/company_dashboard.rb
+++ app/dashboards/company_dashboard.rb
@@ -1,63 +1,47 @@
 require "administrate/base_dashboard"
 
-class CompanyDashboard < Administrate::BaseDashboard
-  # ATTRIBUTE_TYPES
-  # a hash that describes the type of each of the model's fields.
-  #
-  # Each different type represents an Administrate::Field object,
-  # which determines how the attribute is displayed
-  # on pages throughout the dashboard.
+class CompanyDashboard < BaseDashboard
   ATTRIBUTE_TYPES = {
+    practices: Field::HasMany.with_options(limit: 25),
+    asset_classes: Field::HasMany.with_options(limit: 25),
+    job_functions: Field::HasMany.with_options(limit: 25),
+    locations: Field::HasMany.with_options(limit: 25),
     id: Field::Number,
+    is_request: Field::Boolean,
+    url: Field::String,
     name: Field::String,
-    created_at: Field::DateTime,
-    updated_at: Field::DateTime,
-  }.freeze
+  }.merge(SHARED_TYPES).freeze
 
-  # COLLECTION_ATTRIBUTES
-  # an array of attributes that will be displayed on the model's index page.
-  #
-  # By default, it's limited to four items to reduce clutter on index pages.
-  # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
-    id
     name
-    created_at
-    updated_at
+    practices
+    asset_classes
+    job_functions
+    locations
   ].freeze
 
-  # SHOW_PAGE_ATTRIBUTES
-  # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
-    id
     name
-    created_at
-    updated_at
-  ].freeze
+    practices
+    asset_classes
+    job_functions
+    locations
+    url
+  ].concat(SHARED_SHOW_PAGE_ATTRIBUTES).freeze
 
-  # FORM_ATTRIBUTES
-  # an array of attributes that will be displayed
-  # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
     name
+    practices
+    asset_classes
+    job_functions
+    locations
+    url
+    is_request
   ].freeze
 
-  # COLLECTION_FILTERS
-  # a hash that defines filters that can be used while searching via the search
-  # field of the dashboard.
-  #
-  # For example to add an option to search for open resources by typing "open:"
-  # in the search field:
-  #
-  #   COLLECTION_FILTERS = {
-  #     open: ->(resources) { resources.where(open: true) }
-  #   }.freeze
   COLLECTION_FILTERS = {}.freeze
 
-  # Overwrite this method to customize how companies are displayed
-  # across all pages of the admin dashboard.
-  #
-  # def display_resource(company)
-  #   "Company ##{company.id}"
-  # end
+  def display_resource(company)
+    company.name
+  end
 end
