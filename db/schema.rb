ad193964aa6bb8661a9e0071e2b1e3b9f747c1fb
DmytroKondrashov <kondrashovd101@gmail.com>
2023-10-05 14:42:09 +0300

    add object changes to versions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_05_113809) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_05_114114) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
@@ -142,6 +142,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_05_113809) do
     t.string "whodunnit"
     t.text "object"
     t.datetime "created_at"
+    t.text "object_changes"
     t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
