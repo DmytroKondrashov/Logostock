f280ebd06300b3519c726a138c025689987d5061
DmytroKondrashov <kondrashovd101@gmail.com>
2023-10-05 16:25:38 +0300

    add application controller

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,21 +1,18 @@
-# All Administrate controllers inherit from this
-# `Administrate::ApplicationController`, making it the ideal place to put
-# authentication logic or other before_actions.
-#
-# If you want to add pagination or other controller-level concerns,
-# you're free to overwrite the RESTful controller actions.
-module Admin
-  class ApplicationController < Administrate::ApplicationController
-    before_action :authenticate_admin
+class Admin::ApplicationController < Administrate::ApplicationController
+  before_action :authenticate_user!
+  before_action :set_time_zone_from_user
 
-    def authenticate_admin
-      # TODO Add authentication logic here.
-    end
+  # helper ThriveRudyNew::Application.helpers
 
-    # Override this value to specify the number of elements to display at a time
-    # on index pages. Defaults to 20.
-    # def records_per_page
-    #   params[:per_page] || 20
-    # end
+  def records_per_page
+    params[:per_page] || 20
+  end
+
+  def default_sorting_attribute
+    :name
+  end
+
+  def set_time_zone_from_user
+    Time.zone = ActiveSupport::TimeZone.new(current_user.time_zone)
   end
 end
